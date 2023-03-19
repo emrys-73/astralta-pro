a923dddaab5d1373268486c526584710c21c4b9a
Adrian Montoya <adrian.montoya@isaraerospace.com>
2023-03-19 23:24:59 +0100

    changed color of text at home

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,9 @@
 <div class="hero min-h-screen bg-zinc-900">
     <div class="hero-content text-center">
         <div class="max-w-md">
-            <h1 class="text-6xl font-bold">Meet Altara</h1>
+            <h1 class="text-6xl font-bold text-white">Meet Altara</h1>
             <p class="py-2">Astralta's AI Virtual Assistant. </p>
-            <p class="py-6">Altara was built to help you understand our vision as well as get 
+            <p class="py-6 text-white">Altara was built to help you understand our vision as well as get 
                 to know what we can offer for you and your business. 
                 Feel free to explore the full range of Altara's capabilities 
                 before scheduling your free audit call with our team.</p>
