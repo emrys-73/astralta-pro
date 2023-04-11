c4f578de51d60dbe423a960d5d350ccb3e12d614
Adrian Montoya <adrian.montoya@isaraerospace.com>
2023-04-11 09:06:06 +0200

    Adapt key and perso

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -18,7 +18,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Astralta | AI Digital Marketing</title>
-    <meta name="description" content="Astralta is an AI-Powered Digital Marketing Agency that can build an AI-Bot to be an expert virtual assistant for your business or personal brand.">
+    <meta name="description" content="Astralta is an AI-Powered Digital Marketing Agency that can build an AI-Assistant to be an expert for your business or personal brand.">
     <meta name="keywords" content="AI-Chatbot, Digital Marketing Agency Ai-Assistant">
 </svelte:head>
 <Navbar/>
